0200e02897b8f3b5d4f83252568432aa1aa14311
co-troll <https://github.com/co-troll>
2024-05-30 17:51:45 +0900

    DAY43

---
+++ NodeJS/20240530/index.js
@@ -0,0 +1,11 @@
+// nodejs Repl
+// 읽기 --> 해석(실행) --> 출력 --> 반복
+// 레포모드
+// nodejs에서 제공하는 인터페이스
+
+// 레포라는 모드를 켜서 확인해보자
+// 터미널을 열고
+// node
+// const str "hello nodejs";
+// nodejs 런타임 환경 == console.log > nodejs 전역객체에 포함되어 있는 메서드
+// console.log(str);
---
+++ NodeJS/20240530/index3.js
@@ -0,0 +1,6 @@
+// 모듈 가져오기
+// 외부 모듈을 가져올수 있다.
+// require("파일의 경로") 메서드
+const devClass = require("./index2");
+
+console.log(devClass);
